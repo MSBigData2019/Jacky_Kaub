7afcfe42c06ddc6bf630cc8a419790e834929f09
jakaub <jacky.kaub@telecom-paristech.fr>
2018-11-11 22:56:14 +0100

    update

--- INF729_Spark_Hadoop/TP_Spark/TP_ParisTech_2017_2018_starter/src/main/scala/com/sparkProject/Trainer.scala
+++ INF729_Spark_Hadoop/TP_Spark/TP_ParisTech_2017_2018_starter/src/main/scala/com/sparkProject/Trainer.scala
@@ -1,7 +1,16 @@
 package com.sparkProject
 
 import org.apache.spark.SparkConf
+import org.apache.spark.ml.Pipeline
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.feature.RegexTokenizer
+import org.apache.spark.ml.feature.StopWordsRemover
+import org.apache.spark.ml.feature.CountVectorizer
+import org.apache.spark.ml.feature.IDF
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.ml.feature.OneHotEncoder
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.classification.LogisticRegression
 
 
 object Trainer {
@@ -41,6 +50,95 @@ object Trainer {
       ********************************************************************************/
 
     println("hello world ! from Trainer")
+
+    var df = spark.read.parquet("/home/jacky/Jacky_Kaub/INF729_Spark_Hadoop/TP_Spark/prepared_trainingset/")
+
+    val tokenizer = new RegexTokenizer()
+      .setPattern("\\W+")
+      .setGaps(true)
+      .setInputCol("text")
+      .setOutputCol("tokens")
+
+
+    val remover = new StopWordsRemover()
+        .setInputCol("tokens")
+        .setOutputCol("filtered")
+
+    val cvModel = new CountVectorizer()
+      .setInputCol("filtered")
+      .setOutputCol("vect")
+
+    val idf = new IDF()
+      .setInputCol("vect")
+      .setOutputCol("tfidf")
+
+    val indexerCountry = new StringIndexer()
+      .setInputCol("country2")
+      .setOutputCol("country_indexed")
+
+    val indexerCurrency = new StringIndexer()
+      .setInputCol("currency2")
+      .setOutputCol("currency_indexed")
+
+
+    var encoderCountry = new OneHotEncoder()
+      .setInputCol("country_indexed")
+      .setOutputCol("country_onehot")
+
+    var encoderCurrency = new OneHotEncoder()
+      .setInputCol("currency_indexed")
+      .setOutputCol("currency_onehot")
+
+
+    val assembler = new VectorAssembler()
+      .setInputCols(
+        Array(
+          "tfidf",
+          "days_campaign",
+          "hours_prepa",
+          "goal",
+          "country_onehot",
+          "currency_onehot"
+        )
+      )
+      .setOutputCol("features")
+
+    val lr = new LogisticRegression()
+      .setElasticNetParam(0.0)
+      .setFitIntercept(true)
+      .setFeaturesCol("features")
+      .setLabelCol("final_status")
+      .setStandardization(true)
+      .setPredictionCol("predictions")
+      .setRawPredictionCol("raw_predictions")
+      .setThresholds(Array(0.7, 0.3))
+      .setTol(1.0e-6)
+      .setMaxIter(300)
+
+    val pipeline = new Pipeline()
+      .setStages(
+        Array(
+          tokenizer,
+          remover,
+          cvModel,
+          idf,
+          indexerCountry,
+          indexerCurrency,
+          encoderCountry,
+          encoderCurrency,
+          assembler,
+          lr
+        )
+      )
+
+
+    val model =  pipeline.fit(df)
+
+
+    println(s"Total number of rows: ${df.count}")
+    println(s"Number of columns ${df.columns.length}")
+
+    df.show()
 
   }
 }
